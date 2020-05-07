37c6901bdcfc803be064ce208732e9de02b69ffa
Горбуля Артем <gorbulya@gmail.com>
2020-05-07 16:41:05 +0300

    1

--- zadanie1/script.js
+++ zadanie1/script.js
@@ -1,1 +1,23 @@
-alert(1);
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses : {},
+  optionalExpenses : {},
+  income : [],
+  savings : false,
+};
+
+let stat1 = prompt("Введите обязательную статью расходов в этом месяце", '');
+let	value1 = prompt("Во сколько обойдется?", '');
+let stat2 = prompt("Введите обязательную статью расходов в этом месяце", '');
+let	value2 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[stat1] = value1;
+appData.expenses[stat2] = value2;
+
+let budget1Day = money / 30;
+
+alert(budget1Day);
