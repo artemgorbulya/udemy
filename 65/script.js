ade14854015db3f58f6c8dc75492cbd4a983d9f0
Artem <gorbulya@gmail.com>
2020-11-08 20:50:22 +0200

    asdsad

---
+++ 65/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// const ans = prompt('Введите ваше число');
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+const str = 'My name is R2D2';
+console.log(str.match(/\D/ig));
+
+//console.log(ans.search(reg));
+//console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/\./g,'*'));
+
+//console.log('12-34-56'.replace(/-/g, ':'));
+
+
+
+
